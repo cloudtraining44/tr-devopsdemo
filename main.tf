fe76c4b499e8986d84ca0e6ba319d852979c819e
Sohail Mohammed <cloudtraining44@gmail.com>
2023-05-09 18:03:31 -0500

    ec2 terraform

--- main.tf
+++ main.tf
@@ -16,6 +16,16 @@ resource "aws_instance" "app_server" {
 #  count         = 3
   ami           = "ami-0889a44b331db0194"
   instance_type = "t2.micro"
+  user_data = <<EOF
+  #!/bin/bash
+  # Use this for your user data (script from top to bottom)
+  # install httpd (Linux 2 version)
+  yum update -y
+  yum install -y httpd
+  systemctl start httpd
+  systemctl enable httpd
+  echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
+  EOF
 
   tags = {
     Name = "demo-vm"
