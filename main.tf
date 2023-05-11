ca30a008050499b907eef45c4bd4f044cee8decf
Sohail Mohammed <cloudtraining44@gmail.com>
2023-05-11 18:32:05 -0500

    0511

--- main.tf
+++ main.tf
@@ -1,34 +1,58 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-  required_version = ">= 1.2.0"
-}
-
-provider "aws" {
-  region  = "us-east-1"
-}
-
-resource "aws_instance" "app_server" {
-#  count         = 3
-  ami           = "ami-0889a44b331db0194"
-  instance_type = "t2.micro"
-  user_data = <<EOF
-  #!/bin/bash
-  # Use this for your user data (script from top to bottom)
-  # install httpd (Linux 2 version)
-  yum update -y
-  yum install -y httpd
-  systemctl start httpd
-  systemctl enable httpd
-  echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
-  EOF
-
+#EC2 instance using UserData
+resource "aws_instance" "demo-instance" {
+  ami                    = "ami-0889a44b331db0194"
+  instance_type          = "t2.micro"
+  key_name               = "0511demokp"
+  vpc_security_group_ids = [aws_security_group.allow_port80.id]
+  user_data              = "${file("userdata.sh")}"
   tags = {
-    Name = "demo-vm"
+    Name = "demo_from_terraform"
+    Owner = "Terraform"
   }
 }
 
+
+#Security Group Resource to open port 80 
+resource "aws_security_group" "allow_port80" {
+  name        = "allow_port80"
+  description = "Allow Inbound Traffic on Port 80"
+
+  ingress {
+    description      = "Port 80 from Everywhere"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "Port 8080 from Everywhere"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+    ingress {
+    description      = "ssh from Everywhere"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.demo-instance.public_ip
+}
