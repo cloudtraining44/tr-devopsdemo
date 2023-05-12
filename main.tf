3116b61466f070d7b3f32981b1f3d99e57f37ef5
Sohail Mohammed <cloudtraining44@gmail.com>
2023-05-12 18:11:30 -0500

    Modified

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 #EC2 instance using UserData
 
-variable "name" {}
 resource "aws_instance" "demo-instance" {
   ami                    = "ami-0889a44b331db0194"
   instance_type          = "t2.micro"
@@ -8,7 +7,6 @@ resource "aws_instance" "demo-instance" {
   vpc_security_group_ids = [aws_security_group.allow_port80.id]
   user_data              = "${file("userdata.sh")}"
   tags = {
-    Name = var.name
     Owner = "Terraform"
   }
 }
