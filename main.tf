74c03c647a295c1a893a4a17d3bf09142c216989
Sohail Mohammed <cloudtraining44@gmail.com>
2023-05-12 17:43:35 -0500

    Modified

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "demo-instance" {
   ami                    = "ami-0889a44b331db0194"
   instance_type          = "t2.micro"
   key_name               = "demokp0512"
-#  vpc_security_group_ids = [aws_security_group.allow_port80.id]
+  vpc_security_group_ids = [aws_security_group.allow_port80.id]
   user_data              = "${file("userdata.sh")}"
   tags = {
     Name = "from jenkins"
