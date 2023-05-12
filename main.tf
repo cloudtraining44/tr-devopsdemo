05acd3cecb101b920a918c5652e10f638133a634
Sohail Mohammed <cloudtraining44@gmail.com>
2023-05-12 17:29:11 -0500

    Modified

--- main.tf
+++ main.tf
@@ -2,11 +2,65 @@
 resource "aws_instance" "demo-instance" {
   ami                    = "ami-0889a44b331db0194"
   instance_type          = "t2.micro"
-  key_name               = "0511demokp"
+  key_name               = "demokp0512"
 #  vpc_security_group_ids = [aws_security_group.allow_port80.id]
   user_data              = "${file("userdata.sh")}"
   tags = {
     Name = "from jenkins"
     Owner = "Terraform"
   }
+}
+
+#Security Group Resource to open port 80 
+resource "aws_security_group" "allow_port80" {
+  name        = "Jenkins SG"
+  description = "Jenkins SG"
+
+  ingress {
+    description      = "Port 80 from Everywhere"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  #  ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "Port 8080 from Everywhere"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  #  ipv6_cidr_blocks = ["::/0"]
+  }
+
+    ingress {
+    description      = "ssh from Everywhere"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  #  ipv6_cidr_blocks = ["::/0"]
+  }
+
+    ingress {
+    description      = "Port 80 from Everywhere"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  #  ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  # ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.demo-instance.public_ip
 }
---
+++ userdata_jenkins.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+yum update –y
+yum install git -y
+wget https://releases.hashicorp.com/terraform/1.4.6/terraform_1.4.6_linux_amd64.zip
+unzip terraform_1.4.6_linux_amd64.zip -d /usr/local/bin/
+wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+yum upgrade -y
+dnf install java-11-amazon-corretto -y
+yum install jenkins -y
+systemctl enable jenkins
+systemctl start jenkins
+systemctl status jenkins
