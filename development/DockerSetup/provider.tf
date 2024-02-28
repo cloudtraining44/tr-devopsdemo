df06545f9f82be70629903962da296c3e42e356b
Sohail Mohammed <cloudtraining44@gmail.com>
2024-02-28 13:59:39 -0600

    Added DockerSetup

--- development/DockerSetup/provider.tf
+++ development/DockerSetup/provider.tf
@@ -10,6 +10,4 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-#  access_key = var.access_key
-#  secret_key = var.secret_access_key
 }
--- development/DockerSetup/variable.tf
+++ development/DockerSetup/variable.tf
@@ -1,7 +1,3 @@
-#variable "access_key" {}
-
-#variable "secret_access_key" {}
-
 variable "port" {
   type = list
   default = [22,80,443,8080]
