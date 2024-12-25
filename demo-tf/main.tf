2931cd7ea2464a4392865c0f02c02ca71ccb13ad
Sohail Mohammed <cloudtraining44@gmail.com>
2024-12-25 12:38:03 -0600

    Testing

--- demo-tf/main.tf
+++ demo-tf/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "us-east-1"
 }
 
-resource "aws_instance" "demo-vm-2" {
+resource "aws_instance" "demo-vm-1" {
   ami = "ami-012967cc5a8c9f891"
   instance_type = "t2.micro"
 }
